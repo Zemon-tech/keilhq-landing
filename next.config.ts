5aeec94eefc4ce7c17a8569817fc33945b2a596e
Shivang Kandoi <shivangkandoi006@gmail.com>
2026-08-16 16:54:12 +0530

    fix error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost:3000"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
